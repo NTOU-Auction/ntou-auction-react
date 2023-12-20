f459ffc4421677c89dcb954deff156b4f5fc33e9
anthony <sliu6523@gmail.com>
2023-12-20 20:23:42 +0800

    fix: api path

--- next.config.js
+++ next.config.js
@@ -25,6 +25,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
