dc7f3f9d24c9c1baeafdc7e9a94d8ca76f180b77
Partner0487 <partner0487@gmail.com>
2023-12-23 05:05:36 +0800

    feat:商品詳情 & 購物車RWD、購物車提示錯誤訊息

---
+++ src/app/my-favorite/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import MediaCard from '@/components/MediaCard';
+import TuneIcon from '@mui/icons-material/Tune';
+import Fab from '@mui/material/Fab';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Slider from '@mui/material/Slider';
+
+const commodityAPI = "http://localhost:8080/api/v1/product/product/classification";
+
+export default function Favorite() {
+
+  return (
+    <Box sx={{ display: "block", marginTop: "60px" }}>
+      敬請期待
+    </Box>
+  );
+}
