2fa7938ba98014cd30d6074d30adc16a960a6956
Partner0487 <partner0487@gmail.com>
2023-12-26 13:45:09 +0800

    fix:側邊攔未開發功能移除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,7 +109,7 @@ export default function RootLayout({
   ];
 
   const SELLER_CENTER_LINKS = [
-    { text: "賣家中心", href: user ? "/dashbord" : "/sign-in", icon: StorefrontIcon },
+    //{ text: "賣家中心", href: user ? "/dashbord" : "/sign-in", icon: StorefrontIcon },
     { text: "商品管理", href: user ? "/seller-product" : "/sign-in", icon: SellIcon },
     { text: "訂單管理", href: user ? "/order" : "/sign-in", icon: ContentPasteIcon },
     { text: "上架商品", href: user ? "/add-product" : "/sign-in", icon: AddIcon },
@@ -120,7 +120,7 @@ export default function RootLayout({
     { text: "我的訂單", href: user ? "/my-order" : "/sign-in", icon: GradingIcon },
     { text: "購物車", href: user ? "/shopping-cart" : "/sign-in", icon: ShoppingCartIcon },
     { text: "聊天室", href: user ? "/chat" : "/sign-in", icon: ChatIcon },
-    { text: "設定", href: user ? "/" : "/sign-in", icon: SettingsIcon },
+    //{ text: "設定", href: user ? "/" : "/sign-in", icon: SettingsIcon },
   ];
 
   /* 側邊欄收縮 */
